da99f784d7412f1cfac45c508c22e136b4c41dc0
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-06 18:08:23 -0400

    format fields in admin and add search and filter fields

--- despesa/admin.py
+++ despesa/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 
 from .models import Despesa
-
+from .utils import format_despesa_valor
 
 class DespesaAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['format_data_da_despesa', 'captalize_nome', 'format_valor', 'captalize_categoria', 'parcelas']
+    list_filter = ['data_da_despesa', 'categoria', 'parcelas']
+    search_fields = ['nome', 'valor', 'parcelas']
 
+    def format_data_da_despesa(self, obj):
+        format = '%d/%m/%Y'
+        return obj.data_da_despesa.strftime(format)
+    
+    def format_valor(self, obj):
+        return format_despesa_valor(obj)
+
+    def captalize_nome(self, obj):
+        return obj.nome.capitalize()
+    
+    def captalize_categoria(self, obj):
+        return obj.categoria.nome.capitalize()
+    
+    format_data_da_despesa.short_description = 'Data da Despesa'
+    format_valor.short_description = 'Valor'
+    captalize_nome.short_description = 'Nome'
+    captalize_categoria.short_description = 'Categoria'
 
 admin.site.register(Despesa, DespesaAdmin)
