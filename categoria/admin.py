42659cdfcffa411acc62f1081407da353605dda9
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-06 18:07:55 -0400

    format fields in admin and add search fields

--- categoria/admin.py
+++ categoria/admin.py
@@ -4,7 +4,18 @@ from .models import Categoria
 
 
 class CategoriaAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['format_nome', 'format_descricao']
+    search_fields = ['nome', 'descricao']
+
+    def format_nome(self, obj):
+        return obj.nome.capitalize()
+
+    def format_descricao(self, obj):
+        return obj.descricao.capitalize()
+
+    format_nome.short_description = 'Nome'
+    format_descricao.short_description = 'Descrição'
+
 
 
 admin.site.register(Categoria, CategoriaAdmin)
