753e8ea5a4c8a94fc688c49f1d33e51d03389d4d
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-06 18:09:32 -0400

    change field type and function __str__

--- categoria/models.py
+++ categoria/models.py
@@ -4,7 +4,7 @@ class Categoria(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     nome = models.CharField(max_length=255, blank=False, null=False)
-    descricao = models.CharField(max_length=255, blank=True, null=True)
+    descricao = models.TextField(max_length=255, blank=True, null=True)
 
     def __str__(self):
-        return self.nome
+        return self.nome.capitalize()
