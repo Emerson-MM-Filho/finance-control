cc030bdef856dcca9b18d46120e2f7d8a8676c1f
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-06 14:48:15 -0400

    register in admin

--- categoria/admin.py
+++ categoria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Categoria, CategoriaAdmin)
--- despesa/admin.py
+++ despesa/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Despesa
+
+
+class DespesaAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Despesa, DespesaAdmin)
