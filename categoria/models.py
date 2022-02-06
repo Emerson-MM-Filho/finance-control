f57e59117c37168fd32826a0f7d4fae1af611844
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-06 14:13:47 -0400

    create model categoria

--- categoria/models.py
+++ categoria/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Categoria(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    descricao = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
