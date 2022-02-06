ed2fa3ef46bb94db9da296c8c351ecf597ba7d0f
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-06 14:14:15 -0400

    create DespesaSerializer

---
+++ despesa/serializers.py
@@ -0,0 +1,8 @@
+from despesa.models import Despesa
+from rest_framework import serializers
+
+
+class DespesaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Despesa
+        fields = ['nome', 'descricao', 'valor', 'categoria']
