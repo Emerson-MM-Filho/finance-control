390cf5abe763ba235aeae0db34a76ea358c46bc0
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-06 14:14:02 -0400

    create model despesa

--- despesa/models.py
+++ despesa/models.py
@@ -1,3 +1,15 @@
+from pyexpat import model
 from django.db import models
+from categoria.models import Categoria
 
-# Create your models here.
+class Despesa(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    descricao = models.CharField(max_length=255, blank=True, null=True)
+    valor = models.DecimalField(blank=False, null=False, decimal_places=2, max_digits=8)
+    categoria = models.ForeignKey(Categoria, blank=False, null=False, on_delete=models.DO_NOTHING)
+
+
+    def __str__(self):
+        return f'{self.nome} - {self.valor} - {self.categoria}'
