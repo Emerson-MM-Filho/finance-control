6fc9290e95f0b33dd7501ce4e13f46895a744fdf
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-06 14:16:20 -0400

    create GetAllDespesas view

--- despesa/views.py
+++ despesa/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Despesa
+from.serializers import DespesaSerializer
+
+
+class GetAllDespesas(viewsets.ModelViewSet):
+    queryset = Despesa.objects.all().order_by('-created_at')
+    serializer_class = DespesaSerializer
