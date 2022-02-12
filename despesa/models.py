54d51eb01c27db6f615c0a38755501d188360be9
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-12 12:42:53 -0400

    add cartao in despesa models

--- despesa/models.py
+++ despesa/models.py
@@ -1,9 +1,15 @@
 from datetime import datetime
 
 from django.db import models
+from cartao.models import Cartao
 from categoria.models import Categoria
 
 class Despesa(models.Model):
+    tipo_choices = (
+        ('Crédito', 'Crédito'),
+        ('Débito', 'Débito'),
+    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     nome = models.CharField(max_length=255, blank=False, null=False)
@@ -12,6 +18,8 @@ class Despesa(models.Model):
     categoria = models.ForeignKey(Categoria, blank=False, null=False, on_delete=models.DO_NOTHING)
     data_da_despesa = models.DateField(default=datetime.now, blank=False, null=False)
     parcelas = models.IntegerField(null=True, blank=True)
+    cartao = models.ForeignKey(Cartao, null=False, blank=False, on_delete=models.DO_NOTHING, default=Cartao.objects.first().id)
+    tipo = models.CharField(choices=tipo_choices, max_length=255, null=False, blank=False, default=tipo_choices[0])
 
 
     def __str__(self):
