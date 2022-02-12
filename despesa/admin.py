7b4e275a6913c36c9cdcbb28d18f9510ec64181c
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2022-02-12 12:44:42 -0400

    add tipo in admin

--- despesa/admin.py
+++ despesa/admin.py
@@ -4,8 +4,8 @@ from .models import Despesa
 from .utils import format_despesa_valor
 
 class DespesaAdmin(admin.ModelAdmin):
-    list_display = ['format_data_da_despesa', 'captalize_nome', 'format_valor', 'captalize_categoria', 'parcelas']
-    list_filter = ['data_da_despesa', 'categoria', 'parcelas']
+    list_display = ['format_data_da_despesa', 'captalize_nome', 'format_valor', 'captalize_categoria', 'parcelas', 'cartao', 'format_tipo']
+    list_filter = ['data_da_despesa', 'categoria', 'parcelas', 'cartao', 'tipo']
     search_fields = ['nome', 'valor', 'parcelas']
 
     def format_data_da_despesa(self, obj):
@@ -14,6 +14,9 @@ class DespesaAdmin(admin.ModelAdmin):
     
     def format_valor(self, obj):
         return format_despesa_valor(obj)
+
+    def format_tipo(self, obj):
+        return obj.get_tipo_display()
 
     def captalize_nome(self, obj):
         return obj.nome.capitalize()
@@ -25,5 +28,6 @@ class DespesaAdmin(admin.ModelAdmin):
     format_valor.short_description = 'Valor'
     captalize_nome.short_description = 'Nome'
     captalize_categoria.short_description = 'Categoria'
+    format_tipo.short_description = 'Tipo'
 
 admin.site.register(Despesa, DespesaAdmin)
